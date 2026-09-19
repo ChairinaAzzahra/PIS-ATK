a6d32c1d80aa24610360bf4848644a7126847cc8
Chairina250180165 <chairina.250180165@mhs.unimal.ac.id>
2026-09-19 17:02:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,34 @@
 // Panggil file data dan fungsi sesuai materi slide 
 require_once 'Data_barang.php';
 require_once 'Fungsi.php';
+
+// Hitung total nilai stok menggunakan fungsi yang sudah dibuat
+$totalNilaiAset = hitungTotalNilaiStok($daftarBarang);
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistem Informasi Produk</title>
+    <style>
+      body {
+        font-family: sans-serif;
+        margin:20px;
+      }
+      table {
+        border-collapse: collapse;
+        witdh: 100%;
+      }
+      th, td {
+        border: 1px solid #ccc;
+        padding: 8px;
+        text-align: left;
+      }
+      th {
+        background-color:#f2f2f2:
+      }
+      .stok-kritis {
+        background-color: #ffcccc;
+      }
+    </style>
