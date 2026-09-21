1328e28fb6a047f530ac240504cd90b897705acf
Chairina250180165 <chairina.250180165@mhs.unimal.ac.id>
2026-09-21 20:07:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,3 +33,45 @@ $totalNilaiAset = hitungTotalNilaiStok($daftarBarang);
         background-color: #ffcccc;
       }
     </style>
+</head>
+<body>
+
+    <h2>Daftar Inventori Alat Tulis Kampus</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama Barang</th>
+                <th>Kategori</th>
+                <th>Harga</th>
+                <th>Stok</th>
+                <th>Deskripsi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($daftarBarang as $barang): ?> 
+                <?php
+                    // Mengecheck apakah stok berkurang dari 3?
+                ?>
+                <tr class="<?php if ($isKritis) { echo 'stok-kritis'; } ?>">
+                    <td><?php echo $barang['id']; ?></td>
+                    <td><?php echo $barang['nama']; ?></td>
+                    <td><?php echo $barang['kategori']; ?></td>
+                    <td>Rp <?php echo number_format($barang['harga'], 0, ',', '.'); ?></td>
+                    <td>
+                        <?php echo $barang['stok']; ?>
+                        <?php if ($isKritis): ?>
+                            <small style="color: red;">(Stok Menipis!)</small>
+                        <?php endif; ?>
+                    </td>
+                    <td><?php echo $barang['dekripsi']; ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+
+    <h3>Total Nilai Stok Gudang: Rp <?php echo number_format($totalNilaiAset, 0, ',', '.'); ?></h3>
+
+</body>
+</html>
