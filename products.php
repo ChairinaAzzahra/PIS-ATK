24b339d4814532285cd68c6a145cc03d88e9afc3
Chairina250180165 <chairina.250180165@mhs.unimal.ac.id>
2026-09-21 19:41:17 +0700

    Update products.php

--- products.php
+++ products.php
@@ -8,30 +8,30 @@ $daftarBarang = [
         "harga" => 32000,
         "stok" => 5,
         "deskripsi" => "Binder ukuran A5 berisi 20 ring untuk catatan perkuliahan."
-],
-[
+    ],
+    [
         "id" => "ATK-02",
         "nama" => "Pensil Mekanik Pilot 0.5mm",
         "kategori" => "Alat Tulis",
         "harga" => 20000,
-        "stok" => 2,
+        "stok" => 2, // Stok menipis dari < 3
         "deskripsi" => "Pensi mekanik awet dengan grip karet empuk nyaman untuk digunakan."
-],
-[
+    ],
+    [
         "id" => "ATK-03",
         "nama" => "Highlighter Stabilo Boss",
         "kategori" => "Alat Tulis",
         "harga" => 12000,
-        "stok" => 1,
+        "stok" => 1, // Stok menipis dari < 3
         "deskripsi" => "Penanda teks warna kuning pastel."
-],
-[
+    ],
+    [
         "id" => "ATK-04",
         "nama" => "Kertas Refill Binder A5",
         "kategori" => "Alat Tulis",
         "harga" => 15000,
         "stok" => 10,
         "deskripsi" => "Isian binder bergaris berisi 100 lembar."
-]
-;
->
+    ]
+];
+?>
